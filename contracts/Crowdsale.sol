61fcff389984e3080c6cdc72fd712bfa50bace03
Sr Developer <dev.l33@zoho.com>
2018-02-23 23:06:06 +0900

    - separate crowdsale and token contract - update ethusd price via oraclize API - change stage mechanism

---
+++ contracts/Crowdsale.sol
@@ -0,0 +1,243 @@
+pragma solidity ^0.4.18;
+
+import "./Token.sol";
+import "./SafeMath.sol";
+import './Ownable.sol';
+
+/**
+ * @title Crowdsale
+ * @dev Crowdsale is a base contract for managing a token crowdsale,
+ * allowing investors to purchase tokens with ether. This contract implements
+ * such functionality in its most fundamental form and can be extended to provide additional
+ * functionality and/or custom behavior.
+ * The external interface represents the basic interface for purchasing tokens, and conform
+ * the base architecture for crowdsales. They are *not* intended to be modified / overriden.
+ * The internal interface conforms the extensible and modifiable surface of crowdsales. Override 
+ * the methods to add functionality. Consider using 'super' where appropiate to concatenate
+ * behavior.
+ */
+
+contract Crowdsale is Ownable {
+  using SafeMath for uint256;
+
+  // The token being sold
+  Token public token;
+
+  // Address where funds are collected
+  address public wallet;
+
+  // How many token units a buyer gets per wei
+  uint256 public rate;
+
+  // Amount of wei raised
+  uint256 public weiRaised;
+
+  // Amount of token sold
+  uint256 public tokenSold;
+
+  // Stage information
+  uint stageStartDate;
+  uint stageEndDate;
+  uint8 stageNum;
+
+  /// List of Affiliates user address
+  mapping (address => bool) public affiliates;
+  mapping (address => bool) public reviewers;
+
+  /**
+   * Event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+  /**
+   * Event for token allocate logging
+   * @param beneficiary who got the tokens
+   * @param amount amount of tokens allocated
+   */
+  event TokenAllocate(address indexed beneficiary, uint256 amount);
+
+  /**
+   * @param _wallet Address where collected funds will be forwarded to
+   * @param _token Address of the token being sold
+   */
+  function Crowdsale(address _wallet, Token _token) public {
+    require(_wallet != address(0));
+    require(_token != address(0));
+
+    wallet = _wallet;
+    token = _token;
+  }
+
+  // -----------------------------------------
+  // Crowdsale external interface
+  // -----------------------------------------
+
+  /**
+   * @dev fallback function ***DO NOT OVERRIDE***
+   */
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  /**
+   * @dev low level token purchase ***DO NOT OVERRIDE***
+   * @param _beneficiary Address performing the token purchase
+   */
+  function buyTokens(address _beneficiary) public payable {
+
+    uint256 weiAmount = msg.value;
+    _preValidatePurchase(_beneficiary, weiAmount);
+
+    // calculate token amount to be created
+    uint256 tokens = _getTokenAmount(weiAmount);
+
+    tokens += _getBonusAmount(_beneficiary, tokens);
+
+    _processPurchase(_beneficiary, tokens);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+    tokenSold += tokens;
+
+    TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens);
+
+    _forwardFunds();
+  }
+
+  function stage(uint _start, uint _end, uint256 _rate) onlyOwner public returns(uint8) {
+    require(_rate > 0);
+    stageStartDate = _start;
+    stageEndDate = _end;
+    rate = _rate;
+    stageNum++;
+    return stageNum;
+  }
+
+  function allocate(address _to, uint _amount) onlyOwner public {
+    tokenSold += _amount;
+    _deliverTokens(_to, _amount);
+    
+    TokenAllocate(_to, _amount);
+  }
+
+  /// @notice Allow users to buy tokens for `_rate` eth
+  /// @param _rate rate the users can sell to the contract
+  function setRate(uint256 _rate) onlyOwner public {
+      require(_rate > 0);
+      rate = _rate;
+  }
+
+  /**
+  * @dev Function to register affiliates
+  * @param _affiliate The address of affiliate user
+  */
+  function addAffiliate(address _affiliate) onlyOwner public returns(bool) {
+      require(_affiliate != address(0));
+      affiliates[_affiliate] = true;
+
+      return true;
+  }
+
+  /**
+  * @dev Function to register reviewers
+  * @param _reviewer The address of affiliate user
+  */
+  function addReviewer(address _reviewer) onlyOwner public returns(bool) {
+      require(_reviewer != address(0));
+      reviewers[_reviewer] = true;
+
+      return true;
+  }
+
+  /**
+  * @dev Function to remove affiliates
+  * @param _affiliate The address of affiliate user
+  */
+  function removeAffiliate(address _affiliate) onlyOwner public returns(bool) {
+      require(_affiliate != address(0));
+      affiliates[_affiliate] = false;
+
+      return true;
+  }
+
+  /**
+  * @dev Function to remove reviewers
+  * @param _reviewer The address of affiliate user
+  */
+  function removeReviewer(address _reviewer) onlyOwner public returns(bool) {
+      require(_reviewer != address(0));
+      reviewers[_reviewer] = false;
+      
+      return true;
+  }
+
+  // -----------------------------------------
+  // Internal interface (extensible)
+  // -----------------------------------------
+
+  /**
+   * @dev Validation of an incoming purchase. Use require statemens to revert state when conditions are not met. Use super to concatenate validations.
+   * @param _beneficiary Address performing the token purchase
+   * @param _weiAmount Value in wei involved in the purchase
+   */
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) view internal {
+    require(_beneficiary != address(0));
+    require(_weiAmount != 0);
+    require(now >= stageStartDate && now < stageEndDate);
+  }
+
+  /**
+   * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends its tokens.
+   * @param _beneficiary Address performing the token purchase
+   * @param _tokenAmount Number of tokens to be emitted
+   */
+  function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+    token.transfer(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+   * @param _beneficiary Address receiving the tokens
+   * @param _tokenAmount Number of tokens to be purchased
+   */
+  function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
+    _deliverTokens(_beneficiary, _tokenAmount);
+  }
+
+  /**
+   * @dev Override to extend the way in which ether is converted to tokens.
+   * @param _weiAmount Value in wei to be converted into tokens
+   * @return Number of tokens that can be purchased with the specified _weiAmount
+   */
+  function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+    return _weiAmount.mul(rate);
+  }
+
+  function _getBonusAmount(address _beneficiary, uint256 _tokenAmount) internal view returns(uint256) {
+        // send bonus for each ICO Stage
+        uint256 bonus;
+
+        // calculate bonus for Affiliates
+        if (affiliates[_beneficiary]) {
+            bonus += _tokenAmount / 10; // 10% discount for affiliates
+        }
+
+        // calculate reviewer's bonus
+        if (stageNum == 1 && reviewers[_beneficiary]) {
+          bonus += _tokenAmount; // 100% discount for reviewers at Stage1
+        }
+
+        return bonus;
+    }
+
+  /**
+   * @dev Determines how ETH is stored/forwarded on purchases.
+   */
+  function _forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+}
--- contracts/Manger.sol
+++ contracts/Manger.sol
@@ -1,15 +1,22 @@
 pragma solidity ^0.4.18;
 
 import "./Token.sol";
+import "./Crowdsale.sol";
+import "./oraclizeAPI_0.5.sol";
 
-contract Manager is Ownable {
+contract Manager is Ownable, usingOraclize {
 
-    struct ArtistToken {
-        address artistAddress;
-        address contractAddress;
+    struct ArtistICO {
+        address crowdsale;
+        address token;
+        uint price; // USD price i.e, 1 token = 70 ($0.7)
     }
 
-    uint8 constant public DECIMALS = 18;
+    // USD ETH rate 1 eth = x USD ether
+    uint256 public ethusd;
+    
+    // Price update frequency.
+    uint public updatePriceFreq = 2 minutes;
 
     /// allocate 50 million tokens to the artist
     uint256 constant public ALLOCATION_ARTIST = 50000000 ether;
@@ -17,20 +24,22 @@ contract Manager is Ownable {
     /// allocate 50 million tokens to HCR
     uint256 constant public ALLOCATION_HCR = 50000000 ether;
 
-    ArtistToken[] public tokens;
+    mapping (address => ArtistICO) public icos;
+    mapping (uint => address) public artistIndex;
+    uint public icoCount;
 
     /// address of hunter coporation
-    address HCRAddress;
+    address FounderAddress;
 
     /// address of wallet for funds
     address wallet;
 
-    event TokenIssue(address indexed artist, address indexed token);
+    event TokenIssue(address indexed artist, address indexed crowdsale, address indexed token);
 
     function Manager(address _wallet, address _hcrAddress) public {
         require(_wallet != address(0) && _hcrAddress != address(0));
         wallet = _wallet;
-        HCRAddress = _hcrAddress;
+        FounderAddress = _hcrAddress;
     }
 
     /*
@@ -40,61 +49,97 @@ contract Manager is Ownable {
     function createToken(
         address _artistAddress,
         string _name,
-        string _symbol,
-        uint256 _tokenSaleHardCap,
-        uint256 _price,
-        uint256 _saleStartDate,
-        uint _saleStageBonus1,
-        uint _saleStageBonus2,
-        uint _saleStageBonus3,
-        uint _saleStageBonus4
+        string _symbol
     ) onlyOwner public returns(address)
     {
         require(_artistAddress != address(0));
-        Token token = new Token(_name, _symbol, _tokenSaleHardCap, _price, wallet, _saleStartDate, _saleStageBonus1, _saleStageBonus2, _saleStageBonus3, _saleStageBonus4);
+        Token token = new Token(_name, _symbol);
+
+        Crowdsale sale = new Crowdsale(wallet, token);
 
         token.mint(_artistAddress, ALLOCATION_ARTIST);
-        token.mint(HCRAddress, ALLOCATION_HCR);
+        token.mint(FounderAddress, ALLOCATION_HCR);
 
-        address tokenAddress = address(token);
+        icos[_artistAddress] = ArtistICO(address(sale), address(token), 0);
+        artistIndex[icoCount] = _artistAddress;
+        icoCount++;
+        
+        TokenIssue(_artistAddress, address(sale),  address(token));
 
-        ArtistToken memory artistToken = ArtistToken(_artistAddress, tokenAddress);
-        tokens.push(artistToken);
-
-        TokenIssue(_artistAddress, tokenAddress);
-
-        return tokenAddress;
+        return icos[_artistAddress].crowdsale;
     }
 
-    function countTokens() view public returns (uint) {
-        return tokens.length;
+    function setStage(address _artist, uint _start, uint _end, uint256 _amount, uint _usdPrice) onlyOwner public {
+        require(_amount > 0 && _usdPrice > 0);
+        require(icos[_artist].crowdsale != address(0));
+
+        Crowdsale sale = Crowdsale(icos[_artist].crowdsale);
+        Token token = Token(icos[_artist].token);
+        token.mint(icos[_artist].crowdsale, _amount * 1 ether);
+        uint rate = ethusd / _usdPrice;
+        sale.stage(_start, _end, rate);
+
+        icos[_artist].price = _usdPrice;
     }
 
-    function getToken(address artistAddress) view public returns (address) {
-        for (uint i = 0; i < tokens.length; i++) {
-            if (tokens[i].artistAddress == artistAddress) {
-                return tokens[i].contractAddress;
+    function allocate(address _artist, address _to, uint _amount) onlyOwner public {
+        require(icos[_artist].crowdsale != address(0));
+        Crowdsale sale = Crowdsale(icos[_artist].crowdsale);
+        sale.allocate(_to, _amount * 1 ether);
+    }
+
+    function setPrice(address _artist, uint256 _usdPrice) onlyOwner public {
+        require(icos[_artist].crowdsale != address(0));
+        Crowdsale sale = Crowdsale(icos[_artist].crowdsale);
+        uint rate = ethusd / _usdPrice;
+        sale.setRate(rate);
+        icos[_artist].price = _usdPrice;
+    }
+    
+    function setPriceUpdateFreq(uint _freq) onlyOwner public {
+        updatePriceFreq = _freq;
+    }
+
+    function addAffiliate(address _artist, address _user) onlyOwner public {
+        require(icos[_artist].crowdsale != address(0));
+        Crowdsale sale = Crowdsale(icos[_artist].crowdsale);
+        sale.addAffiliate(_user);
+    }
+
+    function addReviewer(address _artist, address _user) onlyOwner public {
+        require(icos[_artist].crowdsale != address(0));
+        Crowdsale sale = Crowdsale(icos[_artist].crowdsale);
+        sale.addReviewer(_user);
+    }
+
+    function getCrowdsale(address _artist) view public returns (address) {
+        return icos[_artist].crowdsale;
+    }
+
+    function getToken(address _artist) view public returns (address) {
+        return icos[_artist].token;
+    }
+
+    function __callback(bytes32 myid, string result) {
+        require(msg.sender == oraclize_cbAddress());
+        ethusd = parseInt(result, 2);
+        _updateICOPrice();
+        updatePrice();
+    }
+
+    function updatePrice() public payable {
+        oraclize_query(updatePriceFreq, "URL", "json(https://api.etherscan.io/api?module=stats&action=ethprice&apikey=YourApiKeyToken).result.ethusd");
+    }
+
+    // -----------------------------------------
+    // Internal interface (extensible)
+    // -----------------------------------------
+    function _updateICOPrice() internal {
+        for (uint i = 0; i < icoCount; i++) {
+            if (icos[artistIndex[i]].price > 0) {
+                Crowdsale sale = Crowdsale(icos[artistIndex[i]].crowdsale);
+                sale.setRate(ethusd / icos[artistIndex[i]].price);
             }
         }
-        return address(0);
     }
-
-    function setRate(address _tokenAddress, uint256 _rate) public {
-        require(_tokenAddress != address(0));
-        Token token = Token(_tokenAddress);
-        token.setRate(_rate);
-    }
-
-    function addAffiliate(address _tokenAddress, address _userAddress) onlyOwner public {
-        require(_tokenAddress != address(0));
-        Token token = Token(_tokenAddress);
-        token.addAffiliate(_userAddress);
-    }
-
-    function addReviewer(address _tokenAddress, address _userAddress) onlyOwner public {
-        require(_tokenAddress != address(0));
-        Token token = Token(_tokenAddress);
-        token.addReviewer(_userAddress);
-    }
-
 }
